cf10bf3c60e935198a6f3ff4d2b119e88afb6e78
g4ss4n <g4ss4n@gmail.com>
2024-09-15 15:23:00 +0300

    app initiation

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+// src/pages/Home/Home.tsx
+import React from 'react';
+import './Home.css';
+
+const Home: React.FC = () => {
+    return (
+        <div className="home">
+            <h2>Home Page</h2>
+            <p>Welcome to the Home Page!</p>
+        </div>
+    );
+};
+
+export default Home;
